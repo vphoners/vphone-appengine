db2b25a393071180ba941a088e491b8139ae5109
mohsen <m.hariri@gmail.com>
2016-07-17 21:36:50 +0200

    improve finding the destination

--- handle_incoming_email.php
+++ handle_incoming_email.php
@@ -4,10 +4,15 @@ $mail_data = file_get_contents('php://input');
 
 echo syslog(LOG_INFO, $mail_data);
 
+function get(&$var, $default=null) {
+    return isset($var) ? $var : $default;
+}
+
 function extract_destination($mail, $header_struct) {
   $header_part = mailparse_msg_get_part($mail, $header_struct);
   $headers = mailparse_msg_get_part_data($header_part);
-  $to_email = $headers["headers"]["delivered-to"];
+  $to_email = get($headers["headers"]["delivered-to"],
+                $headers["headers"]["to"]);
   $to = explode("@", $to_email)[0];
   return $to;
 }
@@ -25,11 +30,12 @@ mailparse_msg_parse($mail, $mail_data);
 $struct = mailparse_msg_get_structure($mail);
 
 $to = extract_destination($mail, $struct[0]);
-$contents = extract_contents($mail, $struct[1], $mail_data);
+if(isset($struct[1]))
+  $contents = extract_contents($mail, $struct[1], $mail_data);
 if(empty($contents))
   $contents = extract_contents($mail, $struct[0], $mail_data);
 
-echo syslog(LOG_INFO, sprintf("sending email to '%s', contents: '%s'",
+echo syslog(LOG_INFO, sprintf("sending sms to '%s', contents: '%s'",
                           $to, $contents));
 
 
