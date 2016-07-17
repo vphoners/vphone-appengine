fea38f4eb4f26ee55c359944dae085827d27e230
mohsen <m.hariri@gmail.com>
2016-07-17 12:31:35 +0200

    allow feras to receive emails for smss

--- handle_incoming_sms.php
+++ handle_incoming_sms.php
@@ -7,8 +7,15 @@ if($_SERVER["CONTENT_TYPE"] != "application/json") {
 
 $data = json_decode(file_get_contents('php://input'), true);
 
-if(isset($data["from"]) && isset($data["timestamp"]) && isset($data["body"])) {
+if($data["device"] == "mohsen") {
+  $to = "m.hariri@gmail.com"
+}else{
+  $to = "feraswilson2010@gmail.com"
+}
 
+if(isset($data["from"]) && isset($data["timestamp"]) && isset($data["body"])) {
+  mail($to , "sms" , $data["body"],
+        "From: " . $data["from"] . "@vphone-1339.appspotmail.com");
 }else {
   header("HTTP/1.1 400 Request is missing required paramters");
 }
