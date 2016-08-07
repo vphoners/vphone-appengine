1ed169965280d5220173667eb1916dac2336227e
mohsen <m.hariri@gmail.com>
2016-08-07 21:45:30 +0200

    redirect users to home page instead of admin interface

--- wordpress/wp-content/themes/vphone/inc/login.php
+++ wordpress/wp-content/themes/vphone/inc/login.php
@@ -12,3 +12,17 @@ function my_login_stylesheet() {
     wp_enqueue_style( 'custom-login', get_stylesheet_directory_uri() . '/css/style-login.css' );
 }
 add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );
+
+add_filter("login_redirect", "custom_login_redirect", 10, 3);
+
+function custom_login_redirect($redirect_to, $request, $user){
+      //is there a user to check?
+     if(is_array($user->roles)){
+           //check for admins
+           if(in_array("administrator", $user->roles)){
+                return home_url("/wp-admin/");
+            }else{
+                 return home_url();
+            }
+     }
+}
